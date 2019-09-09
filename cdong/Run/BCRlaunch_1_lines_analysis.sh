8a11a5264e153d5ad7924f8b6bc9953e83f92151
Chuang Dong <dong@ciml.univ-mrs.fr>
2019-09-09 19:16:43 +0200

    add files

---
+++ cdong/Run/BCRlaunch_1_lines_analysis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+module load userspace/all
+module load python3/3.6.3
+source /home/cdong/snakemake/bin/activate
+
+snakemake --jobs 96 --use-singularity --singularity-args "-B /scratch/cdong:/scratch/cdong" --snakefile snakefile/snakefile_all.yml --configfile config/config_BCR.yml --cluster-config config/cluster.json --cluster 'sbatch -A {cluster.project} --job-name {cluster.job-name} --ntasks {cluster.ntasks} --cpus-per-task {threads} --mem-per-cpu {cluster.mem-per-cpu} --partition {cluster.partition} --time {cluster.time} --mail-user {cluster.mail-user} --mail-type {cluster.mail-type} --error {cluster.error} --output {cluster.output}'
+
